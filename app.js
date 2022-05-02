cc118b603c1d0fe83519b9165dce816aeb306b7f
Cesar Gonzalez <jugador409@hotmail.com>
2022-05-01 23:16:56 -0500

    se configura proyecto

---
+++ app.js
@@ -0,0 +1,18 @@
+const Trello = require("./main");
+
+//var trello = new Trello("key", "token");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+var cardTitle = `Card Nueva ${new Date()}`;
+
+console.log("Ejecutando!");
+
+trello.addCard(cardTitle, "LaunchX Card Description", "IdList",
+  function (error, trelloCard) {
+    if (error) {
+      console.log('Could not add card:', error);
+    }
+    else {
+      console.log('Added card:', trelloCard);
+    }
+  });
